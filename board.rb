f67e4fbde51644d45f90ac2a2bc750f016cee0f3
Laszlo Kiss <pinewood82@gmail.com>
2022-02-22 14:21:48 +0100

    Add def print_board

--- board.rb
+++ board.rb
@@ -1,8 +1,33 @@
 class Board
+  attr_reader :board, :size
   
   def initialize(size = 3)
     @size = size
     @board = Array.new(size) { Array.new(size, '.')}
   end
+
+  def print_board
+    print "\t"
+    print " \u2015" * (size * 2)
+    puts
+    board.each { |row| 
+      print "\t\u2502 "
+      print row * " \u2502 "
+      puts " \u2502"
+      print "\t"
+      print " \u2015" * (size * 2)
+      puts
+    }
+    
+  end
+  
+  def self.clear
+    print "\e[2J\e[f"
+  end
+
   
 end
+
+b = Board.new
+Board.clear
+b.print_board
